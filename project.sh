441014240791e1d2abb2e9de2ff7c60cf0860762
Deon Poncini <dex1337@gmail.com>
2014-11-26 19:44:57 -0800

    Update project generator to add license, readme
    The License is by default MIT, readme is a template
    Also fixed the cmake generation so it actually generates the compile
    commands (no need for make and proper command)
    Fixed up git setup to not use deprecated switch --set-upstream, and also
    use the set-url so I can add additional push targets if desired

--- project.sh
+++ project.sh
@@ -2,7 +2,13 @@
 
 # script for creating a new project at a given directory
 # to use type project.sh <path> <name>
-PD=$1$2
+# $1=path
+# $2=name
+
+PD=$1$2            # project full directory
+PNAME=$2           # project name
+YEAR=$(date +'%Y') # current year
+
 echo 'Setting up project: ' $PD
 mkdir -p $PD
 
@@ -15,7 +21,7 @@ mkdir $PD/include            # include directory
 echo 'Writing build files'
 # make a skeleton CMakeLists.txt file
 echo 'cmake_minimum_required(VERSION 2.8)'       > $PD/CMakeLists.txt
-echo 'project('$2')'                            >> $PD/CMakeLists.txt
+echo 'project('$PNAME')'                        >> $PD/CMakeLists.txt
 echo ''                                         >> $PD/CMakeLists.txt
 echo '################################'         >> $PD/CMakeLists.txt
 echo '# Compiler setup'                         >> $PD/CMakeLists.txt
@@ -38,7 +44,7 @@ echo '################################'         >> $PD/CMakeLists.txt
 echo '# Build targets'                          >> $PD/CMakeLists.txt
 echo '################################'         >> $PD/CMakeLists.txt
 echo ''                                         >> $PD/CMakeLists.txt
-echo 'add_executable('$2                        >> $PD/CMakeLists.txt
+echo 'add_executable('$PNAME                    >> $PD/CMakeLists.txt
 echo '    ${SRC}'                               >> $PD/CMakeLists.txt
 echo '    ${INCLUDES})'                         >> $PD/CMakeLists.txt
 # make a skelton main.cpp file
@@ -51,12 +57,61 @@ echo '    std::cout << "Hello, World!\n";'      >> $PD/src/main.cpp
 echo '    return 0;'                            >> $PD/src/main.cpp
 echo '}'                                        >> $PD/src/main.cpp
 echo ''                                         >> $PD/src/main.cpp
+# make a license file, MIT license
+echo 'The MIT License (MIT)'                                                          >  $PD/LICENSE
+echo ''                                                                               >> $PD/LICENSE
+echo 'Copyright (c) '$YEAR' Deon Poncini'                                             >> $PD/LICENSE
+echo ''                                                                               >> $PD/LICENSE
+echo 'Permission is hereby granted, free of charge, to any person obtaining a copy'   >> $PD/LICENSE
+echo 'of this software and associated documentation files (the "Software"), to deal'  >> $PD/LICENSE
+echo 'in the Software without restriction, including without limitation the rights'   >> $PD/LICENSE
+echo 'to use, copy, modify, merge, publish, distribute, sublicense, and/or sell'      >> $PD/LICENSE
+echo 'copies of the Software, and to permit persons to whom the Software is'          >> $PD/LICENSE
+echo 'furnished to do so, subject to the following conditions:'                       >> $PD/LICENSE
+echo ''                                                                               >> $PD/LICENSE
+echo 'The above copyright notice and this permission notice shall be included in all' >> $PD/LICENSE
+echo 'copies or substantial portions of the Software.'                                >> $PD/LICENSE
+echo ''                                                                               >> $PD/LICENSE
+echo 'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR'     >> $PD/LICENSE
+echo 'IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,'       >> $PD/LICENSE
+echo 'FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE'    >> $PD/LICENSE
+echo 'AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER'         >> $PD/LICENSE
+echo 'LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,'  >> $PD/LICENSE
+echo 'OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE'  >> $PD/LICENSE
+echo 'SOFTWARE.'                                                                      >> $PD/LICENSE
+# make a skeleton README.md
+echo '---'                                                           >  $PD/README.md
+echo 'Title: '$PNAME                                                 >> $PD/README.md
+echo 'Description:'                                                  >> $PD/README.md
+echo 'Author: Deon Poncini'                                          >> $PD/README.md
+echo ''                                                              >> $PD/README.md
+echo '---'                                                           >> $PD/README.md
+echo ''$PNAME                                                        >> $PD/README.md
+echo '==============='                                               >> $PD/README.md
+echo ''                                                              >> $PD/README.md
+echo 'Developed by Deon Poncini <dex1337@gmail.com>'                 >> $PD/README.md
+echo ''                                                              >> $PD/README.md
+echo 'Downloads'                                                     >> $PD/README.md
+echo '---------'                                                     >> $PD/README.md
+echo ''                                                              >> $PD/README.md
+echo 'Description'                                                   >> $PD/README.md
+echo '-----------'                                                   >> $PD/README.md
+echo ''                                                              >> $PD/README.md
+echo 'Building'                                                      >> $PD/README.md
+echo '--------'                                                      >> $PD/README.md
+echo ''                                                              >> $PD/README.md
+echo 'Usage'                                                         >> $PD/README.md
+echo '-----'                                                         >> $PD/README.md
+echo ''                                                              >> $PD/README.md
+echo 'License'                                                       >> $PD/README.md
+echo '-------'                                                       >> $PD/README.md
+echo 'Copyright (c) '$YEAR' Deon Poncini. '                          >> $PD/README.md
+echo 'See the LICENSE file for license rights and limitations (MIT)' >> $PD/README.md
 
 # Generate the compile_commands.json file for auto complete
 echo 'Generating compile_commands.json file'
 pushd $PD/_build
-CXX=cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS="ON"
-make
+cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS="ON"
 cp compile_commands.json $HOME/.ycm/
 popd
 
@@ -72,9 +127,10 @@ git commit -m "Initial commit"
 
 # Setup a remote git repository to store changes
 echo 'Setting up remote git repo'
-ssh deonp@git.sectorsoftware.net "git init --bare git.sectorsoftware.net/$2.git"
-git remote add origin deonp@git.sectorsoftware.net:git.sectorsoftware.net/$2.git
+ssh deonp@git.sectorsoftware.net "git init --bare git.sectorsoftware.net/$PNAME.git"
+git remote add origin deonp@git.sectorsoftware.net:git.sectorsoftware.net/$PNAME.git
+git remote set-url --add --push origin deonp@git.sectorsoftware.net:git.sectorsoftware.net/$PNAME.git
 git push origin master
-git branch --set-upstream master origin/master
-ssh deonp@git.sectorsoftware.net "cd git.sectorsoftware.net/$2.git; git update-server-info"
+git branch --set-upstream-to=origin/master master
+ssh deonp@git.sectorsoftware.net "cd git.sectorsoftware.net/$PNAME.git; git update-server-info"
 popd
